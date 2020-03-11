35462fb680dd1ec28ef8e2eec261c4029bbdb425
Otuman Nkomanya <otuman.nkomanya@athenasoftware.net>
2020-03-10 20:13:53 -0400

    Added assets folder

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -1,5 +1,5 @@
  <!--  Fonts and icons     -->
- <link rel="dns-prefetch" href="//fonts.gstatic.com">
+<!-- <link rel="dns-prefetch" href="//fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">
 <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
